e131b08a491b79131fe93bc938e3f2b586e869de
Jose Villegas <villegasjose.gg@gmail.com>
2017-04-24 16:52:40 -0400

    travis deploy

---
+++ travis_deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+if [ -n "$GITHUB_API_KEY" ]; then
+  echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+  # delete previous changes
+  cd public/
+  rm -r !(.git)
+  cd ..
+  # Build the project.
+  hugo # if using a theme, replace by `hugo -t <yourtheme>`
+
+  # Go To Public folder
+  cd public
+  # Add changes to git.
+  git add -A
+
+  # Commit changes.
+  msg="rebuilding site `date`"
+  if [ $# -eq 1 ]
+    then msg="$1"
+  fi
+  git commit -m "$msg"
+
+  # Push source and build repos.
+  git push -f -q https://jose-villegas:$GITHUB_API_KEY@github.com/jose-villegas/jose-villegas.github.io.git master
+
+  # Come Back
+  cd ..
+fi
